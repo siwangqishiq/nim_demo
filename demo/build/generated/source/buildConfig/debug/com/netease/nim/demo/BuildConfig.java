6278ca784ca5f403956cdd5532f7b403fbcc6e26
siwangqishiq <525647740@qq.com>
2016-04-09 17:54:15 +0800

    Initial commit

---
+++ demo/build/generated/source/buildConfig/debug/com/netease/nim/demo/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.netease.nim.demo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.netease.nim.demo";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 21;
+  public static final String VERSION_NAME = "";
+  // Fields from default config.
+  public static final String BUILD_DATE = "2016-4-9 17:47:54";
+  public static final String GIT_REVISION = "";
+}
